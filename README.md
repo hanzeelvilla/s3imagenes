6589bafa4a1eb88be3c72d37b324c0954f0f8950
Hanzeel Villa <hanzeelvilla0@gmail.com>
2024-10-27 18:03:50 -0600

    conexion al bucket

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Como guardar imágenes en S3
 
+## Requisitos
+1. Cuenta de AWS
+
 ## Configuración
 
 1. Instalar dependencias
@@ -8,7 +11,22 @@
 npm i
 ```
 
-2. Correr el servidor
+2. Crear un bucket en AWS
+3. Crear una nueva política para el servicio S3 con: **GetObject**, **PutObject** y **DeleteObject**
+4. En resources seleccionar el bucket creado y **any** en todos los objetos
+5. Crear un usuario
+6. Agregar la política al usuario
+7. Generar un ID de clave de acceso
+8. Crear el archivo .env
+
+```
+BUCKET_NAME=your_bucket_name
+BUCKET_REGION=your_bucket_region
+ACCESS_KEY_ID=your_access_key_id
+SECRET_ACCESS_KEY=your_secret_access_key
+```
+
+9. Correr el servidor
 
 ```
 node app.js
--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import multer from 'multer';
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import dotenv from 'dotenv';
+import crypto from 'node:crypto'
 
-const storage = multer.memoryStorage()
-const upload = multer({ storage: storage }) // guardar imagen en memoria y no en disco
+const randonImgName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex')
+
+dotenv.config();
+
+const s3 = new S3Client({
+    credentials: {
+        accessKeyId: process.env.ACCESS_KEY_ID,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY
+    },
+    region: process.env.BUCKET_REGION
+});
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage }); // guardar imagen en memoria y no en disco
 
 const app = express();
 app.use(cors()); 
@@ -15,6 +30,18 @@ app.get('/', (req, res) => {
 app.post('/api/posts', upload.single('image') ,async (req, res) => {
     console.log(req .body)
     console.log(req.file)
+
+    // guardar la imagen en el bucket
+    const params = {
+        Bucket: process.env.BUCKET_NAME,
+        Key: randonImgName(),
+        Body: req.file.buffer,
+        ContentType: req.file.mimetype
+    };
+
+    const command = new PutObjectCommand(params);
+    await s3.send(command);
+
     res.json({ message: 'Post created' });
 })
 
